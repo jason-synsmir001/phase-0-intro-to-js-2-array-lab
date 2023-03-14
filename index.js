c2be981bf92a40328157eaf9486e5016e2012bd5
jason synsmir <jason.synsmir001@gmail.com>
2023-03-14 17:49:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat(){
+    var newArray = cats.slice();
+    newArray.pop();
+    return newArray
+}
+function removeFirstCat(){
+    var newArray = cats.slice();
+    newArray.shift();
+    return newArray
+}
